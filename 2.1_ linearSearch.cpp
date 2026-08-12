8cf2fd4aebcbd2b0b2dcd5b76339cf908313e4dc
Kaustubh Raj <kaustubhraj312@gmail.com>
2026-08-12 12:21:59 +0530

    Add files via upload

---
+++ 2.1_ linearSearch.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int linearSearch(vector<int>& arr, int target) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == target)
+            return i;
+    }
+
+    return -1;
+}
